2451127c3bb87e48463f388b4cca22f84dfea606
Samuel Nunn <samuelnunn90@gmail.com>
2020-12-17 16:17:10 -0500

    login form basic complete

---
+++ authenticate-me/frontend/src/components/LoginFormPage/index.js
@@ -0,0 +1,59 @@
+import  { useState } from 'react';
+import './LoginForm.css'
+import * as sessionActions from '../../store/session';
+import { useDispatch, useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+
+function LoginFormPage() {
+    const dispatch = useDispatch();
+    const sessionUser = useSelector(state => state.session.user);
+
+    const [credential, setCredential] = useState('');
+    const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState([]);
+
+    if (sessionUser) return (
+        <Redirect to='/' />
+    );
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setErrors([]);
+        return dispatch(sessionActions.login({ credential, password }))
+            .catch((res) => {
+                if (res.data && res.data.errors) setErrors(res.data.errors);
+            });
+    }
+
+    return (
+        <div className={'page-layout'}>
+        <form onSubmit={handleSubmit} className={'form'}>
+            <ul>
+                {errors.map((error, i) => <li key={i}>{error}</li>)}    
+            </ul>
+            <h2>Login</h2>
+            <label className={'username-email'}>
+                Username or Email
+                <input 
+                type='text'
+                value={credential}
+                onChange={(e) => setCredential(e.target.value)}
+                required
+                />
+            </label>
+            <label>
+                Password
+                <input 
+                type='password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                />
+            </label>
+            <button type="submit" className={'button'}>Log In</button>
+        </form>
+        </div>
+    );
+}
+
+export default LoginFormPage;
